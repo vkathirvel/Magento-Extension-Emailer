941779814fd74f50806dd4a3b7e0b008c18f8f0e
Kathir Vel <sid@kathirvel.com>
2014-02-05 01:50:56 +0000

    0.2.0

--- app/code/community/Optimiseweb/Emailer/Helper/Data.php
+++ app/code/community/Optimiseweb/Emailer/Helper/Data.php
@@ -4,8 +4,8 @@
  * Optimiseweb Emailer Data Helper
  *
  * @package     Optimiseweb_Emailer
- * @author      Sid Vel (sid@optimiseweb.co.uk)
- * @copyright  Copyright (c) 2014 Optimise Web
+ * @author      Kathir Vel (sid@optimiseweb.co.uk)
+ * @copyright   Copyright (c) 2014 Optimise Web
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Optimiseweb_Emailer_Helper_Data extends Optimiseweb_Emailer_Helper_Deprecated
--- app/code/community/Optimiseweb/Emailer/Helper/Deprecated.php
+++ app/code/community/Optimiseweb/Emailer/Helper/Deprecated.php
@@ -4,8 +4,8 @@
  * Optimiseweb Emailer Data Deprecated
  *
  * @package     Optimiseweb_Emailer
- * @author      Sid Vel (sid@optimiseweb.co.uk)
- * @copyright  Copyright (c) 2014 Optimise Web
+ * @author      Kathir Vel (sid@optimiseweb.co.uk)
+ * @copyright   Copyright (c) 2014 Optimise Web
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 class Optimiseweb_Emailer_Helper_Deprecated extends Mage_Core_Helper_Abstract
